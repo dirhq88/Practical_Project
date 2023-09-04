944b19ab4c5420350dcac213920c297f04f18e64
dirhq88 <dirhq88@gmail.com>
2023-09-04 20:23:12 +0900

    create searchLevel and searchWord function

--- src/com/mycom/word/WordCRUD.java
+++ src/com/mycom/word/WordCRUD.java
@@ -75,6 +75,19 @@ public class WordCRUD implements ICRUD{
         return idList;
     }
 
+    public void listAll(int level){
+        int j = 0;
+        System.out.println("----------------------------------------------");
+        for(int i = 0; i< list.size(); i++){
+            int ilevel = list.get(i).getLevel();
+            if(ilevel != level) continue;
+            System.out.print((j+1) + "  ");
+            System.out.println(list.get(i).toString());
+            j++;
+        }
+        System.out.println("----------------------------------------------");
+    }
+
     public void updateItem() {
         System.out.print("=> 수정할 단어 검색 : ");
         String keyword = s.next();
@@ -139,5 +152,17 @@ public class WordCRUD implements ICRUD{
         }
         pr.close();
         System.out.println("==> 데이터 저장 완료!!!");
+    }
+
+    public void searchLevel() {
+        System.out.print("=> 원하는 레벨은? (1~3) ");
+        int level = s.nextInt();
+        listAll(level);
+    }
+
+    public void searchWord() {
+        System.out.print("=> 원하는 단어는? ");
+        String keyword = s.next();
+        listAll(keyword);
     }
 }
